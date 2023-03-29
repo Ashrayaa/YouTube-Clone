4aacf07deb6d119388c7c07b30c41122dd9dfce4
Ashrayaa <mailtoashrayaa@gmail.com>
2023-03-29 17:40:14 +0530

    fixed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import MainContainer from './MainContainer'
 import Sidebar from './Sidebar'
-import WatchPage from './WatchPage'
 
 const Body = () => {
   return (
